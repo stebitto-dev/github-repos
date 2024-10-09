ac93c05d84a1ea828fb7a75dc03b799f11b80bad
Stefano Bitto <stefano.bitto.dev@gmail.com>
2024-10-09 16:47:50 +0200

    Setup Koin

---
+++ app/src/main/java/com/stebitto/github_repos/MainApplication.kt
@@ -0,0 +1,20 @@
+package com.stebitto.github_repos
+
+import android.app.Application
+import com.stebitto.github_repos.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class MainApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger()
+            androidContext(this@MainApplication)
+            modules(appModule)
+        }
+    }
+}
